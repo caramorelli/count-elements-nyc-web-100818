931f99454fc17e7c6edb09e42b35c1cd974db00a
Cara Morelli <caramorelli7@gmail.com>
2018-10-10 02:03:19 +0000

    automatically backed up by learn

--- count_elements.rb
+++ count_elements.rb
@@ -1,6 +1,11 @@
 def count_elements(array)
   count_hash = Hash.new
   array.each do |ele|
+    if count_hash.has_key?(ele)
+      count_hash[ele] += 1 
+    else 
+      count_hash[ele] = 1 
+    end 
   end 
 end
  
